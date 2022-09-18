3abf946496f684b7cba37ab45920108326e1a99e
Chasel <arko9988b@gmail.com>
2022-09-18 17:17:16 +0000

    add test.js

--- NextJS/nextjs/pages/test.js
+++ NextJS/nextjs/pages/test.js
@@ -1,4 +1,12 @@
+
 export default function Test()
 {
-    
+    fetch('http://localhost/api/index')
+    .then(function(response) {
+    return response.json();
+    })
+    .then(function(myJson) {
+    console.log(myJson);
+    });
+
 }
