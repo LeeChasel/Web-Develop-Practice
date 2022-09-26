288965cdda221c44a070177e6ac555083c952907
Chasel <arko9988b@gmail.com>
2022-09-26 12:06:10 +0000

    test fetch method

--- NextJS/nextjs/pages/test_v2.js
+++ NextJS/nextjs/pages/test_v2.js
@@ -4,19 +4,27 @@ import useSWR from 'swr';
 
 const fetcher = (...args) => fetch(...args).then(res => res.json())
 
-export default function Test_v2()
+
+export async function getServerSideProps()
+{
+    const res = await fetch("http://localhost:80/api/index");
+    const data = await res.json();
+
+    return {props:{data}};
+}
+
+export default function Test_v2({data})
 {
 
-    const { data, error } = useSWR('https://jsonplaceholder.typicode.com/todos/1', fetcher)
-    if (error) return <div>Failed to load</div>
-    if (!data) return <div>Loading</div>
-
+    // const { data, error } = useSWR('https://jsonplaceholder.typicode.com/todos/1', fetcher)
+    // if (error) return <div>Failed to load</div>
+    // if (!data) return <div>Loading</div>
+    console.log(data);
     return (
         <div>
-            <h1>
-            {data.name}
-            </h1>
-            <p>{data.bio}</p>
+            {/* <h1>{data.name}</h1>
+            <p>{data.state}</p> */}
+            {data.title}
         </div>
     );
 }
