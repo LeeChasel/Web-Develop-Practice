01a2fc02b9688c4fe52c241280e14f9489d937fb
Chasel <arko9988b@gmail.com>
2022-10-11 18:21:37 +0000

    working on main section go right when sidebar open

--- NextJS/nextjs/components/layout.js
+++ NextJS/nextjs/components/layout.js
@@ -1,8 +1,6 @@
 import Head from 'next/head'
 // import styles from './layout.module.css'
 import Sidebar from './sidebar'
-import Sidebar_demo  from './sidebar_demo'
-import Footer from './footer'
 
 
 export default function Layout({children})
@@ -15,11 +13,9 @@ export default function Layout({children})
                 <link rel="icon" href="/favicon.ico"/>
             </Head>
             <Sidebar/>
-            {/* <Sidebar_demo/> */}
             <main> 
                 {children} 
             </main>
-            {/* <Footer/> */}
         </>
     )
 }
--- NextJS/nextjs/components/sidebar.js
+++ NextJS/nextjs/components/sidebar.js
@@ -1,4 +1,3 @@
-// import React from 'react';
 import {Disclosure} from '@headlessui/react';
 import {Bars3Icon} from '@heroicons/react/24/outline'
 
@@ -10,40 +9,35 @@ function Sidebar()
             <Bars3Icon className="h-6 w-6 block md-hidden" aria-hidden="true"/>
           </Disclosure.Button>
           <Disclosure.Panel className="p-6 w-1/2 h-screen bg-white z-20 fixed top-0 -left-96 lg:left-0 lg:w-60 peer-focus:left-0 peer:transition ease-out delay-150 duration-200">
-            <div className="flex flex-col item-center text-center h-screen">
-            <h1 className="text-base cursor-pointer font-bold text-blue-900 border-b border-gray-100 w-full pb-4">
+            <div className="flex flex-col item-center text-center h-screen relative">
+            <h1 className="cursor-pointer font-bold text-blue-900 border-b border-gray-100 w-full pb-4">
               Nav Bar
             </h1>
             <div className="my-4 border-b border-gray-100 pb-4">
               {/* navbar item */}
               <div className="flex flex-col mb-2  gap-4  hover:bg-gray-900 p-2 rounded-md group hover:shadow-lg m-auto">
-                  <h3 className="text-base text-gray-800 group-hover:text-white font-semibold">
+                  <h3 className="text-gray-800 group-hover:text-white font-semibold">
                   HomePage
                   </h3>
               </div>
               <div className="flex flex-col mb-2  gap-4  hover:bg-gray-900 p-2 rounded-md group hover:shadow-lg m-auto">
-                <h3 className="text-base text-gray-800 group-hover:text-white font-semibold">
+                <h3 className="text-gray-800 group-hover:text-white font-semibold">
                   Modify Data
                 </h3>
               </div>
               <div className="flex flex-col mb-2  gap-4  hover:bg-gray-900 p-2 rounded-md group hover:shadow-lg m-auto">
-                <h3 className="text-base text-gray-800 group-hover:text-white font-semibold">
+                <h3 className="text-gray-800 group-hover:text-white font-semibold">
                   View List
                 </h3>
               </div>
-
             </div>
             {/* Login or Logout button */}
-            <div className="flex my-4  bottom-0  ">
-              <div className="mb-2 items-center  border border-gray-200  hover:bg-gray-900 p-2 rounded-md group cursor-pointer hover:shadow-lg m-auto">
-                <h3 className="text-base text-gray-800 text-center items-center group-hover:text-white font-semibold ">
+            <div className="my-4 bottom-7 absolute mb-2 border border-gray-200  hover:bg-gray-900 p-2 rounded-md group hover:shadow-lg m-auto w-full">
+                <h3 className="text-gray-800 group-hover:text-white font-semibold ">
                   Logout
                 </h3>
-              </div>
             </div>
-            
             </div>
-            
           </Disclosure.Panel>
         </Disclosure>
     );
