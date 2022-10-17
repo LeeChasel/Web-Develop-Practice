dc2f40d637ac1e84746ecf433b7edde6350cb940
Chasel <arko9988b@gmail.com>
2022-10-17 18:09:57 +0000

    edit layout

--- NextJS/nextjs/components/layout.js
+++ NextJS/nextjs/components/layout.js
@@ -5,28 +5,28 @@ import Sidebar from './sidebar'
 function Layout({children})
 {
     // https://github.com/vercel/next.js/issues/5354
-    if (typeof window !== 'undefined')
-    {
-        var state = "close";
-        var sidebarButton = document.getElementById("headlessui-disclosure-button-:R4m:");
-        sidebarButton.addEventListener("click", function() {
-            if (state == "close")
-            {
-                state = "open";
-            } else if (state == "open")
-            {
-                state = "close";
-            } else {
-                state = "error";
-            }
-        });
-    }
+    // if (typeof window !== 'undefined')
+    // {
+    //     var state = "close";
+    //     var sidebarButton = document.getElementById("headlessui-disclosure-button-:R4m:");
+    //     sidebarButton.addEventListener("click", function() {
+    //         if (state == "close")
+    //         {
+    //             state = "open";
+    //         } else if (state == "open")
+    //         {
+    //             state = "close";
+    //         } else {
+    //             state = "error";
+    //         }
+    //     });
+    // }
     // https://github.com/tailwindlabs/tailwindcss/discussions/3461
-    const bgColor = {
-        default : 'bg-black',
-        'close' : 'bg-green-500',
-        'open' : 'bg-yellow-500',
-    }
+    // const bgColor = {
+    //     default : 'bg-black',
+    //     'close' : 'bg-green-500',
+    //     'open' : 'bg-yellow-500',
+    // }
 
     return (
         <>
@@ -35,10 +35,13 @@ function Layout({children})
                 <meta name="description" content="create by NextJs"/>
                 <link rel="icon" href="/favicon.ico"/>
             </Head>
-            <Sidebar/>
+            
             <main>
-                <div className="bg-red-400">
+                <div className="relative min-h-screen bg-red-300 flex flex-col justify-center py-6 sm:py-12">
+                  <div class="bg-white">
+                <Sidebar/>
                 {children}
+                  </div>
                 </div>
                 
             </main>
