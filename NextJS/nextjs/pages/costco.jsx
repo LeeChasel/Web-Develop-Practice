2260a79eabe61b1e7086b70ade15e49f175ca462
Chasel <arko9988b@gmail.com>
2023-05-17 08:19:50 +0000

    create tables to browse tables data

--- NextJS/nextjs/pages/costco.jsx
+++ NextJS/nextjs/pages/costco.jsx
@@ -4,25 +4,24 @@ import { useRouter } from 'next/router';
 function DB_Table({tables})
 {
     const router = useRouter();
-
     return (
-        <div className="overflow-x-auto">
-            <table className="table w-full">
-                <thead>
-                <tr>
-                    {/* <th></th> */}
-                    <th>Table</th>
-                </tr>
-                </thead>
-                <tbody>
-                {tables.map(table => (
-                <tr key={table.Tables_in_costco} onClick={() => router.push('costco/' + table.Tables_in_costco)} className="cursor-pointer">
-                    {/* <th>1</th> */}
-                    <td>{table.Tables_in_costco}</td>
-                </tr>
-                ))}
-                </tbody>
-            </table>
+        <div className="flex justify-center h-full py-5">
+            <div className="overflow-y-auto h-full w-1/3 rounded-md">
+                <table className="table w-full h-full bg-gray-100">
+                    <thead>
+                    <tr>
+                        <th className="text-black bg-zinc-100 border-b-2 border-b-zinc-300">Table Name</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {tables.map(table => (
+                    <tr key={table.Tables_in_costco} onClick={() => router.push('costco/' + table.Tables_in_costco)} className="cursor-pointer">
+                        <td className="text-black bg-zinc-100 border-zinc-300 hover:bg-zinc-200">{table.Tables_in_costco}</td>
+                    </tr>
+                    ))}
+                    </tbody>
+                </table>
+            </div>
         </div>
     )
 }
@@ -34,7 +33,7 @@ function Home()
     if (error) return <div>Failed to load</div>
     if (!data) return <div>Loading...</div>
     return (    
-        <div>
+        <div className="h-full">
             <DB_Table tables={data}/>
         </div>
     )
--- NextJS/nextjs/pages/costco/[db_name].jsx
+++ NextJS/nextjs/pages/costco/[db_name].jsx
@@ -4,43 +4,54 @@ import useSWR from 'swr'
 function Data_Table({data})
 {
     return (
-        <div className="overflow-x-auto">
-            <table className="table w-full">
-                <thead>
-                <tr>
-                    <th>Id</th>
-                    <th>Name</th>
-                    <th>price</th>
-                    <th>stock</th>
-                    <th>type</th>
-                </tr>
-                </thead>
-                <tbody>
-                {data.map(row => (
-                    <tr key={row.id}>
-                        <th>{row.id}</th>
-                        <td>{row.name}</td>
-                        <td>{row.price}</td>
-                        <td>{row.stock}</td>
-                        <td>{row.type}</td>
+        <div className="flex justify-center h-full p-5">
+            <div className="overflow-y-auto h-full w-full rounded-md">
+                <table className="table w-full h-full bg-gray-100">
+                    <thead>
+                    <tr>
+                        <th className="text-black bg-zinc-100 border-b-2 border-b-zinc-300">ID</th>
+                        <th className="text-black bg-zinc-100 border-b-2 border-b-zinc-300">Name</th>
+                        <th className="text-black bg-zinc-100 border-b-2 border-b-zinc-300">Price</th>
+                        <th className="text-black bg-zinc-100 border-b-2 border-b-zinc-300">Stock</th>
+                        <th className="text-black bg-zinc-100 border-b-2 border-b-zinc-300">Type</th>
                     </tr>
-                ))}
-                </tbody>
-            </table>
+                    </thead>
+                    <tbody>
+                    {data.map(row => (
+                        <tr key={row.id}>
+                            <td className="text-black bg-zinc-100 border-zinc-300">{row.id}</td>
+                            <td className="text-black bg-zinc-100 border-zinc-300">{row.name}</td>
+                            <td className="text-black bg-zinc-100 border-zinc-300">{row.price}</td>
+                            <td className="text-black bg-zinc-100 border-zinc-300">{row.stock}</td>
+                            <td className="text-black bg-zinc-100 border-zinc-300">{row.type}</td>
+                        </tr>
+                    ))}
+                    </tbody>
+                </table>
+            </div>
         </div>
     )
 }
 
-const fetcher = (...args) => fetch(...args).then(res => res.json())
+function BtnActions()
+{
+    return (
+        <div>aa</div>
+    )
+
+}
+
 
 function Home()
 {
     const router = useRouter();
+    const fetcher = (...args) => fetch(...args).then(res => res.json())
     const { data, error } = useSWR('http://localhost/api/costco/' + router.query.db_name + '/index', fetcher)
     if (error) return <div>Failed to load</div>
     if (!data) return <div>Loading...</div>
     return (    
-        <div>
+        <div className="flex flex-col h-full">
+            <BtnActions/>
             <Data_Table data={data}/>
         </div>
     )
